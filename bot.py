98730b45638ba2f3f93444483aa149a1a491fb0c
kuncarajati <gregoriusmahendra041@gmail.com>
2024-06-30 10:47:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,56 @@
-import discord 
-from main import * 
+import discord
+from discord.ext import commands 
+from main import gen_pass 
+import random
+import os
+import requests
  
 # Membaca token dari file token.txt 
 with open("token.txt", "r") as f: 
     token = f.read() 
  
-import discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-@bot.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
+intents = discord.Intents.default() 
+intents.message_content = True 
+ 
+bot = commands.Bot(command_prefix='$', intents=intents) 
+ 
+@bot.event 
+async def on_ready(): 
+    print(f'We have logged in as {bot.user}') 
+ 
+@bot.command() 
+async def hello(ctx): 
+    await ctx.send(f'Hi! I am a bot {bot.user}!') 
+ 
+@bot.command() 
+async def heh(ctx, count_heh = 5): 
+    await ctx.send("he" * count_heh) 
+ 
+@bot.command() 
+async def passw(ctx, panjang = 5): 
+    await ctx.send(gen_pass(panjang)) 
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
+async def mem(ctx):
+    img_name = random.choice(os.listdir("images"))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
 
-@bot.command()
-async def passw(ctx, long = 5):
-    await ctx.send(genn_pass(panjang))
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
 
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+ 
 bot.run(token)
+
+
+
